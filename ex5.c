f4ecf7c2fb7ba74f6da9ec3a955094f067e1a6e1
jessewmc <jessewmc@gmail.com>
2014-07-22 07:53:13 -0600

    initial version of ex5

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*comments bgitshdchksl*/
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  //commenting more
+  printf("yo is %d miles away\n", distance);
+
+  return 0;
+}
