93ca5607e50c978fb9ee3d265af76d2a63ad34ed
jessewmc <jessewmc@gmail.com>
2014-07-22 08:36:25 -0600

    Ex6 initial

---
+++ ex6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+  float power = 2.345f;
+  double super_power = 56789.4532;
+  char initial = 'A';
+  char first_name[] = "joe";
+  char last_name[] = "schmoe";
+
+  printf("yo is %d miles away\n", distance);
+  printf("yo ihave %f levels of power\n", power);
+  printf("yo have %f super powers yeeezy\n", super_power);
+  printf("yo have initial %c\n", initial);
+  printf("yo have first name %s\n", first_name);
+  printf("yo have last name %s\n", last_name);
+  printf("yo whole name be %s %c %s\n", first_name, initial, last_name);
+
+  return 0;
+}
