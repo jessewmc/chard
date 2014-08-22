4f97823b5fb0e6b63baeabaef85b244f6043c380
jessewmc <jessewmc@gmail.com>
2014-08-22 15:50:46 -0600

    test

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct entry{
+  int item;
+  struct entry* next;
+}
+
+struct entry* stack_push(struct entry* stack){
+}
+
+struct entry* stack_pop(struct entry* stack){
+}
+
+
